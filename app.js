4b7af8fd334311dff6b623c67624afddf1679105
Spencer Dedrick <spencer.dedrick@gmail.com>
2022-04-29 10:01:28 -0500

    completed 5 kyu paginationHelper

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ PaginationHelper.prototype.itemCount = function () {
 
 // returns the number of pages
 PaginationHelper.prototype.pageCount = function () {
-  return Math.floor(this.collection.length / this.itemsPerPage);
+  return Math.floor(this.collection.length / this.itemsPerPage + 1);
 };
 
 // returns the number of items on the current page. page_index is zero based.
@@ -55,25 +55,27 @@ PaginationHelper.prototype.pageItemCount = function (pageIndex) {
 // determines what page an item is on. Zero based indexes
 // this method should return -1 for itemIndex values that are out of range
 PaginationHelper.prototype.pageIndex = function (itemIndex) {
-  if (itemIndex >= this.collection.length || pageIndex < 0) {
+  if (itemIndex >= this.collection.length || itemIndex < 0) {
     return -1;
   } else {
-    return Math.floor(itemIndex / this.itemsPerPage + 1);
+    return Math.floor(itemIndex / this.itemsPerPage );
   }
 };
 
 var helper = new PaginationHelper(["a", "b", "c", "d", "e", "f"], 4);
-/*
-helper.pageCount(); //should == 2
-helper.itemCount(); //should == 6
-helper.pageItemCount(0); //should == 4
-helper.pageItemCount(1); // last page - should == 2
-helper.pageItemCount(2); // should == -1 since the page is invalid
-helper.pageIndex(5); //should == 1 (zero based index)
-helper.pageIndex(2); //should == 0
-helper.pageIndex(20); //should == -1
-helper.pageIndex(-10); //should == -1 
+console.log(
 
-*/
+//helper.pageCount(), //should == 2
+//helper.itemCount(), //should == 6
+//helper.pageItemCount(0), //should == 4
+//helper.pageItemCount(1), // last page - should == 2
+//helper.pageItemCount(2), // should == -1 since the page is invalid
+//helper.pageIndex(5), //should == 1 (zero based index)
+helper.pageIndex(2), //should == 0
+//.pageIndex(20), //should == -1
+//helper.pageIndex(-10), //should == -1 
 
-console.log(helper.pageIndex(6));
+
+)
+
+
